80e9d8ebe112874e059e7158699c8afdd1bb1274
Nikolay <nickcozlovsky@yandex.ru>
2023-02-26 18:55:30 +0300

    sdf.

--- main.cs
+++ main.cs
@@ -6,9 +6,11 @@ namespace Git
 	{
 		static void Main (string[] args)
 		{
-			System.Console.WriteLine("Enter your name: ");
-			string name = Console.ReadLine();
+			System.Console.WriteLine("Enter your first name: ");
+			string first_name = Console.ReadLine();
 			System.Console.WriteLine(name.Length); //debug line
+			string last_name = Console.ReadLine();
+			System.Console.WriteLine($"Hello {first_name} {last_name}!");
 		}
 	}
 }
